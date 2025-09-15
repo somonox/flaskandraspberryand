a28ededf42cc27efc1cd9f96a0f9438f5c3672a7
a7a7-7 <hungmin090929@gmail.com>
2025-09-15 17:51:26 +0900

    heheheha

--- ch02/static/index.js
+++ ch02/static/index.js
@@ -1,8 +1,15 @@
 let ddong = 0;
 counter = document.getElementById("counter")
 cbutton = document.getElementById("cbutton")
+sbutton = document.getElementById("sbutton")
 cbutton.addEventListener("click", () => {
     ddong++;
     console.log(ddong)
     counter.innerText = ddong;
+})
+
+sbutton.addEventListener("click", function() {
+    location.href='http://0.0.0.0:5050/'+ ddong;
+    counter.innerText= 0;
+    ddong = 0;
 })
